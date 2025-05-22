e73ae3c74e0d4d20cd41f90153ce29532287e458
Blockmaster00 <benjamin.heinold@gmail.com>
2025-05-22 17:13:44 +0200

    added blueprint

--- main.lua
+++ main.lua
@@ -1,16 +1,36 @@
-local playerData = {}
+local playerDataTable = {}
 
-tm.physics.AddTexture("blueprints/rotationStructure.png", "rotationStructure")
+tm.physics.AddTexture("blueprints/cameraTracker.png", "rotationStructure")
+
+--#region IDEAS / TODO
+--[[
+    - replace 'InitRotationStructure' with 'spawnRotationStructure'
+
+    - add a function to spawn the cursor Game object
+
+    -
+    ]]
 
 function onPlayerJoined(player)
     local playerId = player.playerId
     local playerData = {
         freeCam = false,
         lastPosition = nil,
-        cameraPosition = nil,
-        cameraDirection = tm.quaternion.Create(0, 0, 0, 1),
-        rotationStructure = nil,
+
+        camera = {
+            position = nil,
+            rotation = nil,
+            speed = 1,
+        },
+
+        rotationStructure = {
+            structure = nil,
+            structureId = nil,
+            cursorBlock = nil
+        },
+
         cursor = nil,
+
         input = {
             forward = false,  --w
             backward = false, --s
@@ -22,32 +42,27 @@ function onPlayerJoined(player)
     }
 
     -- Store the player data in a global table
-    playerData[playerId] = playerData
+    playerDataTable[playerId] = playerData
 
-
-    -- Initialize the Rotation Structure and Cursor Block
-    Init_RotationStructure(playerId)
+    -- Initialize player input
+    Init_playerInput(playerId)
 end
 
 tm.players.OnPlayerJoined.add(onPlayerJoined)
 
+--[[
 function update()
     local players = tm.players.CurrentPlayers()
 
     for _, player in ipairs(players) do
-        local playerId = player.playerId
-        local playerData = playerData[playerId]
-
-        if playerData.freeCam then
-            -- Update the camera position and direction
-            PlayerUpdate(player)
-        end
+        PlayerUpdate(player)
     end
 end
+]]
 
 function PlayerUpdate(player)
     local playerId = player.playerId
-    local playerData = playerData[playerId]
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         if not tm.players.IsPlayerInSeat(playerId) then
@@ -92,9 +107,13 @@ function PlayerUpdate(player)
     end
 end
 
-function Init_RotationStructure(playerId)
-    local playerData = playerData[playerId]
-    local structureId = "rStructure_" .. playerId
+function Init_Cursor(playerId)
+end
+
+function Spawn_RotationStructure(playerId)
+    tm.os.Log("Spawn_RotationStructure called for playerId: " .. playerId)
+    local playerData = playerDataTable[playerId]
+    local structureId = "rStructure_" .. playerId ..
 
     -- Create a new structure for the player
     tm.players.SpawnStructure(
@@ -107,9 +126,9 @@ function Init_RotationStructure(playerId)
 
     -- locate and save the cursor block
     local cursorBlock
-    local blockList = tm.players.GetSpawnedStructureById(structureId).GetBlocks()
+    local blockList = tm.players.GetSpawnedStructureById(structureId)[1].GetBlocks()
     for i, block in ipairs(blockList) do
-        if block.GetName() == "t" then
+        if block.GetName() == "PFB_MixelEye_Sphere [Server]" then
             cursorBlock = block
             break
         end
@@ -117,13 +136,11 @@ function Init_RotationStructure(playerId)
 
     -- Store the structure in the player data
     playerData.rotationStructure = {
-        structure = tm.players.GetSpawnedStructureById(structureId),
+        structure = tm.players.GetSpawnedStructureById(structureId)[1],
         structureId = structureId,
         cursorBlock = cursorBlock
     }
 end
-
-
 
 --#region MATH FUNCTIONS
 
@@ -165,7 +182,9 @@ end
 --#region PLAYER INPUT
 
 function Init_playerInput(playerId)
-    local playerData = playerData[playerId]
+    local playerData = playerDataTable[playerId]
+
+    tm.playerUI.AddSubtleMessageForPlayer(playerId, "FreeCam", "Press <sprite index=155> to toggle Free Camera!", 10)
 
     tm.input.RegisterFunctionToKeyDownCallback(playerId,
         "ToggleFreeCam", "v")
@@ -184,122 +203,115 @@ function Init_playerInput(playerId)
 end
 
 function ToggleFreeCam(playerId)
-    local playerData = playerData[playerId]
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
+        tm.os.Log("FreeCam deactivated for playerId: " .. playerId)
         playerData.freeCam = false
+        playerData.rotationStructure.structure.Dispose()
+        playerData.rotationStructure = nil
         tm.players.GetPlayerTransform(playerId).SetPosition(playerData.lastPosition)
         tm.playerUI.AddSubtleMessageForPlayer(playerId, "FreeCam", "Free Camera Deactivated!", 3)
     else
+        tm.os.Log("FreeCam activated for playerId: " .. playerId)
         playerData.freeCam = true
         playerData.lastPosition = tm.players.GetPlayerTransform(playerId).GetPosition()
+        Spawn_RotationStructure(playerId)
         tm.players.PlacePlayerInSeat(playerId, playerData.rotationStructure.structureId)
         tm.playerUI.AddSubtleMessageForPlayer(playerId, "FreeCam", "Free Camera Activated!", 3)
     end
 end
 
-function W_up(callbackData)
-    local playerId = callbackData.playerId
-    local playerData = playerData[playerId]
+function W_up(playerId)
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         playerData.input.forward = false
     end
 end
 
-function W_down(callbackData)
-    local playerId = callbackData.playerId
-    local playerData = playerData[playerId]
+function W_down(playerId)
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         playerData.input.forward = true
     end
 end
 
-function S_up(callbackData)
-    local playerId = callbackData.playerId
-    local playerData = playerData[playerId]
+function S_up(playerId)
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         playerData.input.backward = false
     end
 end
 
-function S_down(callbackData)
-    local playerId = callbackData.playerId
-    local playerData = playerData[playerId]
+function S_down(playerId)
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         playerData.input.backward = true
     end
 end
 
-function A_up(callbackData)
-    local playerId = callbackData.playerId
-    local playerData = playerData[playerId]
+function A_up(playerId)
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         playerData.input.left = false
     end
 end
 
-function A_down(callbackData)
-    local playerId = callbackData.playerId
-    local playerData = playerData[playerId]
+function A_down(playerId)
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         playerData.input.left = true
     end
 end
 
-function D_up(callbackData)
-    local playerId = callbackData.playerId
-    local playerData = playerData[playerId]
+function D_up(playerId)
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         playerData.input.right = false
     end
 end
 
-function D_down(callbackData)
-    local playerId = callbackData.playerId
-    local playerData = playerData[playerId]
+function D_down(playerId)
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         playerData.input.right = true
     end
 end
 
-function Space_up(callbackData)
-    local playerId = callbackData.playerId
-    local playerData = playerData[playerId]
+function Space_up(playerId)
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         playerData.input.up = false
     end
 end
 
-function Space_down(callbackData)
-    local playerId = callbackData.playerId
-    local playerData = playerData[playerId]
+function Space_down(playerId)
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         playerData.input.up = true
     end
 end
 
-function Shift_up(callbackData)
-    local playerId = callbackData.playerId
-    local playerData = playerData[playerId]
+function Shift_up(playerId)
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         playerData.input.down = false
     end
 end
 
-function Shift_down(callbackData)
-    local playerId = callbackData.playerId
-    local playerData = playerData[playerId]
+function Shift_down(playerId)
+    local playerData = playerDataTable[playerId]
 
     if playerData.freeCam then
         playerData.input.down = true
